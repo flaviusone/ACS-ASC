3067cb24356fba42bee5cfba1753173e231d210f
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2014-03-19 12:35:21 +0200

    [Tema_1] Facut partea de swap

--- Tema1/skel/tema/thread_helper.py
+++ Tema1/skel/tema/thread_helper.py
@@ -1,6 +1,6 @@
 import sys
 import random
-from threading import Event,Thread, current_thread, Lock, Semaphore
+from threading import Event,Thread, current_thread, Lock, Semaphore, Condition
 from time import sleep
 from barrier import ReusableBarrierSem
 
@@ -12,45 +12,97 @@ class Master(Thread):
 		self.datastore = datastore
 		self.nodes = nodes
 		self.thread_list = []
-		self.barrier = ReusableBarrierSem(matrix_size)
+		self.self_node = self.nodes[self.node_id]
 		random.seed(0)
 
-		# Aici primesc date de la celalalte noduri
-		# Initializez cu 0
+		#########################
+		# Creez o bariera locala
+		if self.node_id == 0:
+			# initializez bariera
+			self.barrier = ReusableBarrierSem(self.matrix_size)
+		else:
+			# setez bariera comuna
+			self.barrier = self.nodes[0].thread.barrier
+		#########################
+
+		#########################
+		# in valor_pivot primesc date de la celalalte noduri
+		# in incomming_buffer primesc de la alt nod un rand intreg
+		# in outgoing_buffer creez pachetul pe care il trimit la alt nod
 		self.valori_pivot = []
+		self.incomming_buffer = []
+		self.outgoing_buffer = []
+		# Initializez cu 0
 		for i in range(self.matrix_size):
 			self.valori_pivot.append(0)
+			self.incomming_buffer.append(0)
+			self.outgoing_buffer.append(0)
+		#########################
 
 	def run(self):
 		# main for loop
 		# self.datastore.get_A(self.nodes[self.node_id],self.node_id)
-		for j in range(len(self.nodes)):
+		for j in range(self.matrix_size):
 			rp = self.find_pivot(j)
+			self.swap_rows(rp,j)
 
+	def swap_rows(self,rp,j):
+		"""
+			Row j swapps with row rp
+		"""
+		if self.node_id == j:
+			# Creez buffer pt trimitere
+			for i in range(self.matrix_size):
+				self.outgoing_buffer[i] = self.datastore.get_A(self.self_node,i)
+			#Trimit
+			self.nodes[rp].thread.incomming_buffer = self.outgoing_buffer
+
+		if self.node_id == rp:
+			# Creez buffer pt trimitere
+			for i in range(self.matrix_size):
+				self.outgoing_buffer[i] = self.datastore.get_A(self.self_node,i)
+			# Trimit
+			self.nodes[j].thread.incomming_buffer = self.outgoing_buffer
+		self.barrier.wait()
+		return True
 
 	def find_pivot(self,row):
 		"""
 			Finds pivot
+			@type row: Integer
+			@param row: randul pe care se cauta elementul de pe pozitia row
+
+			@return: index-ul rowului care are pivotul
 		"""
+		index = -1
+
 		if self.node_id != row:
-			# Send element from column row to Node row
-			self.send_data(self.nodes[row],self.datastore.get_A(self.nodes[self.node_id],row))
-			sleep(random.randint(1,3))
+			# Trimit element de pe coloana row la nodul row
+			self.send_data_pivot(self.nodes[row],self.datastore.get_A(self.self_node,row))
+
+		# Astept sa primesc toate elementele de la celelalte noduri
 		self.barrier.wait()
-		print "Thread printing stuff"
+
 		if self.node_id == row:
+			# Pun si valoarea mea in vector
+			self.valori_pivot[self.node_id] = self.datastore.get_A(self.self_node,row)
+
 			# Compute local max
 			maxim = -1
-			for k in self.valori_pivot:
-				if k > maxim:
-					maxim = k
+			index = -1
+			for k in range(len(self.valori_pivot)):
+				if abs(self.valori_pivot[k]) > maxim:
+					maxim = self.valori_pivot[k]
+					index = k
+			print "Am calculat maxim %d pe row %d" % (maxim,index)
+			# print maxim
+
+		return index
 
 
-
-		return self.datastore.A_row
-
-	def send_data(self,destination_node,data):
+	def send_data_pivot(self,destination_node,data):
 		destination_node.thread.valori_pivot[self.node_id] = data
+		# .append((data,self.node_id))
 
 
 

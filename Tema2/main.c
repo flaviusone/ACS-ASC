654b86e888f1fbeb8ec2a78be929f4db4a8eff15
Flavius Tirnacop <flavius.tirnacop@cti.pub.ro>
2014-04-06 20:04:37 +0300

    [Tema_2] Schimbat size cu n

--- Tema2/main.c
+++ Tema2/main.c
@@ -3,27 +3,27 @@
 #include <sys/time.h>
 // #include "cblas.h"
 
-void print_matrix(double *mat,unsigned int size){
+void print_matrix(double *mat,unsigned int n){
 	unsigned int i,j;
-	for(i=0;i<size;i++){
-		for(j=0;j<size;j++){
-			printf("%5.2f\t",mat[j+i*size] );
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++){
+			printf("%5.2f\t",mat[j+i*n] );
 		}
 		printf("\n");
 	}
 }
 
-double* create_matrix(unsigned int size){
+double* create_matrix(unsigned int n){
 	double *mat;
 	unsigned int i,j;
-	mat = calloc(size*size, sizeof(double));
+	mat = calloc(n*n, sizeof(double));
 
 	srand(time(NULL));
 
 	//o umplu  cu 1 superior triunghiular
-	for(i=0;i<size;i++){
-		for(j=i;j<size;j++){
-			mat[j+i*size]=rand();
+	for(i=0;i<n;i++){
+		for(j=i;j<n;j++){
+			mat[j+i*n]=rand();
 		}
 	}
 
@@ -32,17 +32,17 @@ double* create_matrix(unsigned int size){
 
 int main(int argc,const char* argv[]){
 	unsigned int i,j;
-	unsigned int size = atoi(argv[1]);
+	unsigned int n = atoi(argv[1]);
 	struct timeval start,finish;
 	double t;
 
 	// aloc si initializez matrice
-	double* mat = create_matrix(size);
+	double* mat = create_matrix(n);
 
 	srand(time(NULL));
 	// Initializez vector
-	double *vec = malloc(size*sizeof(double));
-	for(i=0;i<size;i++)
+	double *vec = malloc(n*sizeof(double));
+	for(i=0;i<n;i++)
 		vec[i]=rand();
 
 	/*------------------------------FUCNTIA MEA------------------------------*/
@@ -59,7 +59,7 @@ int main(int argc,const char* argv[]){
 	printf("Timp Functia mea = %lf\n", t);
 	/*------------------------------FUCNTIA MEA------------------------------*/
 
-
+	my_dtrmv()
 
 	/*------------------------------FUNCTIE BLAS------------------------------*/
 	gettimeofday(&start,0);
@@ -75,7 +75,7 @@ int main(int argc,const char* argv[]){
 	printf("Timp Functie BLAS = %lf\n", t);
 	/*------------------------------FUNCTIE BLAS------------------------------*/
 
-	print_matrix(mat,size);
+	print_matrix(mat,n);
 
 	// Fac inmultirea
 	// for ( i = 0; i < m; i++ )
@@ -87,7 +87,7 @@ int main(int argc,const char* argv[]){
 
 	printf("\n");
 	// Printez rezutlat
-	for(i=0;i<size;i++)
+	for(i=0;i<n;i++)
 		printf("%5.2f ",vec[i]);
 	printf("\n");
 
